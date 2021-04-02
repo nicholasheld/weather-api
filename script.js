5a3db2e3e71b324a0c823edd65d3fe7a2412e4f4
nicholas <nicholaspheld@gmail.com>
2021-04-02 11:24:51 -0500

    updated js to dynamically create cards and display the date with moment

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var cityBox1 = document.querySelector('#cityId');
 var temp = document.querySelector('#temp');
 var humi = $('#humi');
 var wind = $('#wind');
-var uv =$('#uv');
+var uv = $('#uv');
+const html = (strings, ...values) => new DOMParser().parseFromString(strings.map((string, i) => strings[i] + values[i]).join(''), "text/html").body.firstChild;
 
 $(document).ready(() => {
 
@@ -28,11 +29,11 @@ $(document).ready(() => {
                 createTableRow.appendChild(tableData);
                 tableBody.appendChild(createTableRow);
                 //This changes the city name to what was searched
-                cityBox1.append(city);
+                cityBox1.append(city + (moment().format("MMM Do YY")));
                 //attempting to append temp 1 in box 1
-                temp.append(result.main.temp + ' F');
+                temp.append(result.main.temp + ' °F');
                 humi.append(result.main.humidity + ' %');
-                wind.append(result.wind.speed + ' mph')
+                wind.append(result.wind.speed + ' MPH')
                 uv.append(result.main.temp);
 
 
@@ -42,16 +43,29 @@ $(document).ready(() => {
         forecast(city);
     })
     function forecast(search) {
+        $("#box2").empty();
         $.ajax({
             url: `https://api.openweathermap.org/data/2.5/forecast?q=${search}&units=imperial&appid=ddb3fdcf857d1a6f4caecf9c07c0201d`,
             success: function (result) {
                 console.log(result);
-                for (i = 0; i < result.list.length; i++) {
+                for (var i = 0; i < result.list.length; i++) {
 
                     if (result.list[i].dt_txt.indexOf("12:00:00") !== -1) {
                         console.log(result.list[i]);
-                        //create card dynamically and append to - check boostap outermost card if card body works
-                        var card = $('<div>').addClass("card");
+                        //create card dynamically and append to - check boostap outermost card if card body works 
+                        var card = html`
+
+                        <div class="card text-white bg-primary mb-3" style="width: 18rem;">
+                        <div class="card-header"> Date: ${result.list[i].clouds.dt_txt} </div>
+                        <ul class="list-group list-group-flush">
+                        <li class="list-group-item text-white bg-primary mb-3"> Temp: ${result.list[i].main.temp} + ' °F'</li>
+                        <li class="list-group-item text-white bg-primary mb-3">Humidity: ${result.list[i].main.humidity}+ ' %'</li>
+                      </ul>
+                        
+                    </div>`;
+
+                        $("#box2").append(card);
+
                     }
 
                 }
